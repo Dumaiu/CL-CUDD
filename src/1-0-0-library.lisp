1a80b25b09f6674f9bbbb24ca151cd07848db410
J. Jakes-Schauer <dymaio@gmail.com>
2021-09-02 18:18:49 -0400

    1-0-0-library.lisp: [FIXME] hack the shared lib loc

--- src/1-0-0-library.lisp
+++ src/1-0-0-library.lisp
@@ -2,7 +2,7 @@
 (in-package :cl-cudd.baseapi)
 
 (define-foreign-library libcudd
-  (t (:default "libcudd")))
+  (t (:default "/usr/local/lib/libcudd" #|"libcudd"|#)))
 
 (use-foreign-library libcudd)
 
