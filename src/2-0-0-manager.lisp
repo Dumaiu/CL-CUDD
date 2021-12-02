3c593af257c3bf7381f776b87f4b48cce8a6097e
J. Jakes-Schauer <Dymaio@gmail.com>
2021-12-02 13:35:24 -0500

    0-package.lisp,2-0-0-manager.lisp: Check (cudd-check-zero-ref) in DdManager finalizer

--- src/2-0-0-manager.lisp
+++ src/2-0-0-manager.lisp
@@ -5,13 +5,13 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (define-constant +manager-initarg-defaults+
-	  '((initial-num-vars 0)
-		(initial-num-vars-z 0)
-		(initial-num-slots 256)
-		(cache-size 262144)
-		(max-memory 0))
-	:documentation "Used by (manager-init), (with-manager)."
-	:test #'equal))
+      '((initial-num-vars 0)
+        (initial-num-vars-z 0)
+        (initial-num-slots 256)
+        (cache-size 262144)
+        (max-memory 0))
+    :documentation "Used by (manager-init), (with-manager)."
+    :test #'equal))
 
 (defstruct manager
   "A boxed CUDD manager class"
@@ -23,31 +23,36 @@
   ;; This is added since each dd-node is considered unique and
   ;; it is ugly when there are multiple lisp node objects for a single dd-node pointer.
   (node-hash #.`(tg:make-weak-hash-table :weakness :value
-										 ,@(when (featurep :sb-thread)
-											 '(:synchronized t)))
+                                         ,@(when (featurep :sb-thread)
+                                             '(:synchronized t)))
    :type hash-table))
 
 (define-symbol-macro %mp% (manager-pointer *manager*))
 
 (defun manager-init #.`(&key
-						,@+manager-initarg-defaults+)
+                        ,@+manager-initarg-defaults+)
   (with-cudd-critical-section
-	(let* ((p (cudd-init initial-num-vars
-						 initial-num-vars-z
-						 initial-num-slots
-						 cache-size
-						 max-memory))
-		   (m (make-manager :pointer p)))
-	  ;; see 2-4-hook.lisp
-	  (cudd-add-hook p (callback before-gc-hook) :cudd-pre-gc-hook)
-	  (cudd-add-hook p (callback after-gc-hook) :cudd-post-gc-hook)
-	  (cudd-add-hook p (callback before-gc-hook) :cudd-pre-reordering-hook)
-	  (cudd-add-hook p (callback after-gc-hook) :cudd-post-reordering-hook)
-	  (tg:finalize m (lambda ()
-					   (with-cudd-critical-section
-						 (format *error-output* "~&freeing a cudd manager at ~a~%" p)
-						 (cudd-quit p))))
-	  m)))
+    (let* ((p (cudd-init initial-num-vars
+                         initial-num-vars-z
+                         initial-num-slots
+                         cache-size
+                         max-memory))
+           (m (make-manager :pointer p)))
+      ;; see 2-4-hook.lisp
+      (cudd-add-hook p (callback before-gc-hook) :cudd-pre-gc-hook)
+      (cudd-add-hook p (callback after-gc-hook) :cudd-post-gc-hook)
+      (cudd-add-hook p (callback before-gc-hook) :cudd-pre-reordering-hook)
+      (cudd-add-hook p (callback after-gc-hook) :cudd-post-reordering-hook)
+      (finalize m (lambda ()
+                       (with-cudd-critical-section
+                         (format *error-output* "~&freeing a cudd manager at ~a~%" p)
+                         (let ((undead-node-count (cudd-check-zero-ref p)))
+                           (declare (fixnum undead-node-count)) ; TODO: Better type
+                           (assert (zerop undead-node-count) (p undead-node-count)
+                                   "Assert failed in finalizer of manager ~A, with ~D unrecovered nodes (should be 0)."
+                                   p undead-node-count))
+                         (cudd-quit p))))
+      m)))
 
 (defvar *manager* nil "The current manager.
 
@@ -56,10 +61,10 @@ Every function in this package works with this manager.
 Bound to a global manager by default.")
 
 (defmacro with-manager (#.`(&rest
-							  keys
-							&key
-							  ,@+manager-initarg-defaults+)
-						&body body)
+                              keys
+                            &key
+                              ,@+manager-initarg-defaults+)
+                        &body body)
   "Bind a freshly generated manager to *MANAGER*.
 This macro is not so useful when multiple managers are in place.
 Also, all data on the diagram are lost when it exits the scope of WITH-MANAGER.
@@ -78,12 +83,12 @@ Also, all data on the diagram are lost when it exits the scope of WITH-MANAGER.
 "
 
   (declare (ignorable initial-num-vars
-					  initial-num-vars-z
-					  initial-num-slots
-					  cache-size
-					  max-memory))
+                      initial-num-vars-z
+                      initial-num-slots
+                      cache-size
+                      max-memory))
   `(let ((*manager* (manager-init ,@keys)))
-	 ,@body))
+     ,@body))
 
 (defun info (&optional (manager *manager*))
   (declare (type manager manager))
