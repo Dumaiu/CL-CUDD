cbbf2cde5b3a132636c7aac8893c50511b5cd8cd
J. Jakes-Schauer <Dymaio@gmail.com>
2022-02-03 15:51:49 -0500

    2-0-0-manager.lisp: WS

--- src/2-0-0-manager.lisp
+++ src/2-0-0-manager.lisp
@@ -16,8 +16,8 @@
 (defstruct manager
   "A boxed CUDD manager class"
   (pointer (error "MANAGER needs to wrap a pointer")
-           :type cffi:foreign-pointer)
-  
+   :type cffi:foreign-pointer)
+
   ;; Added on 3/5 2018.
   ;; It stores a mapping between a node pointer <-> a lisp node.
   ;; This is added since each dd-node is considered unique and
@@ -25,29 +25,25 @@
   (node-hash #.(append '(tg:make-weak-hash-table :weakness :value)
 					   (when (featurep :sb-thread)
 						 '(:synchronized t)))
-             :type hash-table))
+   :type hash-table))
 
 (define-symbol-macro %mp% (manager-pointer *manager*))
 
-(defun manager-init (&key
-                       (initial-num-vars 0)
-                       (initial-num-vars-z 0)
-                       (initial-num-slots 256)
-                       (cache-size 262144)
-                       (max-memory 0))
+(defun manager-init #.`(&key
+						,@+manager-initarg-defaults+)
   (let* ((p (cudd-init initial-num-vars
-                       initial-num-vars-z
-                       initial-num-slots
-                       cache-size
-                       max-memory))
-         (m (make-manager :pointer p)))
-    ;; see 2-4-hook.lisp
-    (cudd-add-hook p (callback before-gc-hook) :cudd-pre-gc-hook)
-    (cudd-add-hook p (callback after-gc-hook) :cudd-post-gc-hook)
-    (cudd-add-hook p (callback before-gc-hook) :cudd-pre-reordering-hook)
-    (cudd-add-hook p (callback after-gc-hook) :cudd-post-reordering-hook)
-    (tg:finalize m (lambda () (format *error-output* "~&freeing a cudd manager at ~a~%" p) (cudd-quit p)))
-    m))
+					   initial-num-vars-z
+					   initial-num-slots
+					   cache-size
+					   max-memory))
+		 (m (make-manager :pointer p)))
+	;; see 2-4-hook.lisp
+	(cudd-add-hook p (callback before-gc-hook) :cudd-pre-gc-hook)
+	(cudd-add-hook p (callback after-gc-hook) :cudd-post-gc-hook)
+	(cudd-add-hook p (callback before-gc-hook) :cudd-pre-reordering-hook)
+	(cudd-add-hook p (callback after-gc-hook) :cudd-post-reordering-hook)
+	(tg:finalize m (lambda () (format *error-output* "~&freeing a cudd manager at ~a~%" p) (cudd-quit p)))
+	m))
 
 (defvar *manager* nil "The current manager.
 
@@ -55,15 +51,11 @@ Every function in this package works with this manager.
 
 Bound to a global manager by default.")
 
-(defmacro with-manager ((&rest
-                         keys
-                         &key
-                         (initial-num-vars 0)
-                         (initial-num-vars-z 0)
-                         (initial-num-slots 256)
-                         (cache-size 262144)
-                         (max-memory 0))
-                        &body body)
+(defmacro with-manager (#.`(&rest
+							  keys
+							&key
+							  ,@+manager-initarg-defaults+)
+						&body body)
   "Bind a freshly generated manager to *MANAGER*.
 This macro is not so useful when multiple managers are in place.
 Also, all data on the diagram are lost when it exits the scope of WITH-MANAGER.
@@ -82,12 +74,12 @@ Also, all data on the diagram are lost when it exits the scope of WITH-MANAGER.
 "
 
   (declare (ignorable initial-num-vars
-                      initial-num-vars-z
-                      initial-num-slots
-                      cache-size
-                      max-memory))
+					  initial-num-vars-z
+					  initial-num-slots
+					  cache-size
+					  max-memory))
   `(let ((*manager* (manager-init ,@keys)))
-     ,@body))
+	 ,@body))
 
 (defun info (&optional (manager *manager*))
   (declare (type manager manager))
