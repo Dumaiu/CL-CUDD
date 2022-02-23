575651f952636b703c509f09d55c376782bbcffb
J. Jakes-Schauer <Dymaio@gmail.com>
2022-02-22 21:15:50 -0500

    2-config.lisp: docs

--- src/2-config.lisp
+++ src/2-config.lisp
@@ -1,8 +1,10 @@
-;;;; Dynamic configuration variables
+;;;; Dynamic configuration variables.  See ../README.md.
 (in-package :cudd)
 
 (defvar config/guard-pointer-access t
-  "TODO: Disable on high optimization | (safety 0).")
+  #.(format nil "TODO: Disable on high optimization | (safety 0).
+  * TODO: When changed, flag ASDF system for rebuilding?  Or at least this file (~A)."
+            (current-lisp-file-pathname)))
 
 (defvar config/enable-gc t
   "When true, new nodes get equipped with finalizers.")
