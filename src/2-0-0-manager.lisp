7fe2651c842237f3f348ce167e9fa09dfdca05d6
J. Jakes-Schauer <dymaio@gmail.com>
2022-03-09 11:50:43 -0500

    2-0-0-manager.lisp: Minor: don't write to *error-output* twice

--- src/2-0-0-manager.lisp
+++ src/2-0-0-manager.lisp
@@ -251,7 +251,7 @@
 										;; NB: Don't retain a reference to the containing `manager' in this finalizer.  See Masataro Asai's NOTE on the finalizer for `node'.
 										#.(let ((fmt '("~&Freeing CUDD manager #~D at ~a~%" manager-index p)))
 											`(progn
-											   (format *error-output* ,@fmt)
+											   (format *stderr* ,@fmt)
 											   (log-msg :debug :logger cudd-logger ,@fmt)))
 
 										(let ((undead-node-count (cudd-check-zero-ref p)))
