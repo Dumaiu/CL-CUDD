0cf43b13056979048be8b8b0c2ccf69f2985093b
J. Jakes-Schauer <Dymaio@gmail.com>
2022-05-18 11:38:37 -0400

    2-6-dot.lisp: Thread-safety for (plot)

--- src/2-6-dot.lisp
+++ src/2-6-dot.lisp
@@ -1,24 +1,25 @@
-
 (in-package :cudd)
 
 (declaim (reentrant plot))
-(defun plot (pathname node)
+(defun plot (pathname node &aux (manager (node-manager node)))
   "Create '<PATHNAME>.dot' and process it to '<PATHNAME>.pdf'.  Requires that Graphviz be installed.
   * TODO [2021-09-21 Tue]: Raise a specialized exception when `uiop:run-program' fails.
 "
-  ;; (setq pathname (uiop:ensure-absolute-pathname pathname))
+  (declare (manager manager))
   (let ((dot (namestring (make-pathname :type "dot" :defaults pathname)))
         (pdf (namestring (make-pathname :type "pdf" :defaults pathname))))
-	;; (break)
-    (etypecase node
-      ((or add-node bdd-node)
-       (cl-cudd.baseapi:dump-dot
-        (manager-pointer *manager*)
-        (cudd-regular (node-pointer node))
-        dot))
-      (zdd-node
-       (cl-cudd.baseapi:zdd-dump-dot
-        (manager-pointer *manager*)
-        (cudd-regular (node-pointer node))
-        dot)))
+    ;; (break)
+    (with-cudd-critical-section (:manager manager)
+      (let ((mp (manager-pointer manager))
+            (node-ptr (node-pointer node)))
+        (declare (manager-pointer mp)
+                 (node-pointer node-ptr))
+        (etypecase node
+          ((or add-node bdd-node)
+           (cl-cudd.baseapi:dump-dot mp
+                                     (cudd-regular node-ptr)
+                                     dot))
+          (zdd-node
+           (cl-cudd.baseapi:zdd-dump-dot mp (cudd-regular node-ptr)
+                                         dot)))))
     (run-program `("dot" ,dot "-Tpdf" "-o" ,pdf))))
